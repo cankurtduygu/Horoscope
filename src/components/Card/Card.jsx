f0fad6fd2160cef9d681e5d4875a532789af44a2
Duygu cankurt <dyg.cankurt17@gmail.com>
2026-01-14 11:27:37 +0100

    add card component

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -8,8 +8,11 @@ const Card = ({data}) => {
     <div className='card'>  
         {data.map((item)=>(
             <div className='card__item' key={item.id}>
-                <h2 className='card__item__name'>{item.title}</h2>
-                <img className='card__item__img' src={item.image} alt=""/>
+                <h2 className='card__item__title'>{item.title}</h2>
+                <div className='card__item__img' 
+                style={{ backgroundImage: `url(${item.image})`, backgroundSize: 'cover', backgroundPosition: 'center', width: '100%', height: '500px' }}>
+                </div>
+                <p className='card__item__date'>{item.date}</p>
                 
                 <p className='card__item__desc'>{item.desc}</p>
             </div>
